57711848a758d8c9a2a2060a14d4c6ad6966f088
ics20-063 <ics20-063@camosun.bc.ca>
2021-04-16 07:33:57 -0700

    Initial Commit

---
+++ Lab10_Map/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lab10_Map
+//
+//  Created by ICS 224 on 2021-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab10_Map/Lab10_MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab10_MapApp.swift
+//  Lab10_Map
+//
+//  Created by ICS 224 on 2021-04-16.
+//
+
+import SwiftUI
+
+@main
+struct Lab10_MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
